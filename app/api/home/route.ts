29c31ca972ae3e2654289d7af20b4a833fa1b6e3
DeStRaaM <20destraam03@gmail.com>
2023-09-27 21:35:39 +0200

    api

--- app/api/home/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export interface ResponseData {
-	message: string;
-}
-
-export default function handler(res: NextApiResponse) {
-	res.status(200).json({ message: 'hello world!' });
-}
