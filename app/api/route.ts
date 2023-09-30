3a600e3a77026c576c51602e0824ffaa63204da8
DeStRaaM <20destraam03@gmail.com>
2023-09-30 16:24:10 +0200

    fix

--- app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function helloWorldHandler(req: NextApiRequest, res: NextApiResponse) {
-	if (req.method === 'GET') {
-		res.status(200).json({ text: 'Hello' });
-	}
-}
