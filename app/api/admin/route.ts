c69865db9cf0e406b25a782946ea5ceb097677a6
DeStRaaM <20destraam03@gmail.com>
2023-10-12 15:02:48 +0200

    admin page api

---
+++ app/api/admin/route.ts
@@ -0,0 +1,22 @@
+import { NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
+import prisma from '../../../lib/prisma';
+
+export async function GET(res: NextApiResponse) {
+	const posts = await prisma.post.findMany();
+	const products = await prisma.product.findMany();
+	const types = await prisma.type.findMany();
+	const grapes = await prisma.grape.findMany();
+	const countries = await prisma.country.findMany();
+	let json_response = {
+		status: 'success',
+		data: {
+			posts,
+			products,
+			types,
+			grapes,
+			countries,
+		},
+	};
+	return NextResponse.json(json_response);
+}
