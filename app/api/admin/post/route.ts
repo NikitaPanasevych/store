497eb9798ae3593a56a2654f929ef4e564b19411
DeStRaaM <20destraam03@gmail.com>
2023-10-16 21:07:31 +0200

    cloudinary image upload

--- app/api/admin/post/route.ts
+++ app/api/admin/post/route.ts
@@ -1,19 +1,44 @@
 import prisma from '@/lib/prisma';
 import { Post } from '@/models/home.posts';
 import { NextResponse } from 'next/server';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export async function POST(req: Request) {
 	const data: Post = await req.json();
 	const { title, content, image } = data;
+
+	if (!title || !content || !image) return NextResponse.json({ message: 'Please fill all fields' });
+
 	try {
-		const post = await prisma.post.create({
+		const options = {
+			use_filename: true,
+			unique_filename: false,
+			overwrite: true,
+			transformation: [{ width: 1000, height: 752, crop: 'scale' }],
+		};
+		console.log(image, options);
+		try {
+			const result = await cloudinary.uploader.upload(image, options);
+			console.log(result);
+		} catch (err) {
+			console.log(err);
+		}
+
+		/*const post = await prisma.post.create({
 			data: {
 				title,
 				content,
-				image,
+				image: result.secure_url,
 			},
 		});
 		return NextResponse.json({ message: 'Post created successfully', post });
+	}*/
 	} catch (err) {
 		return NextResponse.json({ message: 'Error', err });
 	}
--- lib/hooks/use-create-post.ts
+++ lib/hooks/use-create-post.ts
@@ -1,10 +1,19 @@
 import { Post } from '@/models/home.posts';
 import { useFormik } from 'formik';
 import postSchema from '@/schemas/post.schema';
+import { toast } from 'react-toastify';
 
 export default function useCreatePost() {
-	const onSubmit = (values: Post) => {
-		console.log(values);
+	const onSubmit = async (values: Post) => {
+		await fetch('/api/admin/post', {
+			method: 'POST',
+			body: JSON.stringify(values),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => toast.success('Post created successfully'))
+			.catch((err) => toast.error('Error', err));
 	};
 
 	const { values, errors, touched, handleChange, handleSubmit, handleBlur } = useFormik({
